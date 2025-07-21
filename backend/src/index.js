2e17136d2fc2f961e202b952288e6b40a1143e82
Anushka Sharma <as2401alw@gmail.com>
2025-07-21 20:48:11 +0530

    Initial fullstack chat app

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,18 +2,17 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-
 import path from "path";
 
 import { connectDB } from "./lib/db.js";
-
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5001;
+
 const __dirname = path.resolve();
 
 app.use(express.json());
